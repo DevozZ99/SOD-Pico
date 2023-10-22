cd37ee255c21950b0be34230a6df2579a2c14d0d
DevozZ99 <devozzcraft@gmail.com>
2023-10-22 19:00:31 +0200

    Transition to CircuitPython displayio

--- C/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo make clean
-sudo make -j 8
-sudo ./main 1.12v3
--- prova.py
+++ prova.py
@@ -1,14 +1,20 @@
 import os
 import sys
+import time
 import argparse
 import subprocess
 import requests
 from datetime import datetime
-import time
-
 from threading import Thread
 
 from apa102 import APA102
+
+import board
+import busio
+import displayio
+import terminalio
+from adafruit_display_text import label
+import adafruit_displayio_sh1107
 
 from picovoice import Picovoice
 from pvrecorder import PvRecorder
@@ -39,27 +45,14 @@ ROOMS = {
 num_led = 3
 led_driver = APA102(num_led=num_led)
 
-oled_exe = "./C/main"
+displayio.release_displays()
+i2c = board.I2C()
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
+WIDTH = 128
+HEIGHT = 128
+display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT, display_offset=adafruit_displayio_sh1107.DISPLAY_OFFSET_ADAFRUIT_128x128_OLED_5297, rotation=180)
 
-class CApp:
-    _instance = None
-    
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._process = None
-        return cls._instance
-    
-    def start_c_app(self, arguments):
-        sudo_command = f"sudo -S {oled_exe} {' '.join(arguments)}"
-        if self._process is None:
-            self._process = subprocess.Popen(sudo_command, shell=True, text=True)
-            self._process.wait()
-            self._process.kill()
-            self._process = None
-        else:
-            print("C Application is already running")
-    
+
 class PiVoice(Thread):
 
     def __init__(self, keyword_path, context_path, access_key, device_index, recorder, porcupine_sensitivity=0.75, rhino_sensitivity=0.25):
@@ -76,7 +69,6 @@ class PiVoice(Thread):
         self._default_brightness = 20
         self._device_index = device_index
         self.recorder = recorder
-        self.cApp = CApp()
         
         for i in range(0, num_led):
             led_driver.set_brightness(i, self._default_brightness)
@@ -104,6 +96,25 @@ class PiVoice(Thread):
             for i in range(0, num_led):
                 led_driver.set_pixel(i, RGB_COLORS[new_color][0], RGB_COLORS[new_color][1], RGB_COLORS[new_color][2])
         led_driver.show()
+    
+    def show_image(self, path):
+        group = displayio.Group()
+        bitmap = displayio.OnDiskBitmap(path)
+        tile_grid = displayio.TileGrid(bitmap, pixel_shader = bitmap.pixel_shader)
+        group.append(tile_grid)
+        display.show(group)
+        time.sleep(3)
+        group = displayio.Group()
+        display.show(group)
+        
+    def show_text(self, text):
+        group = displayio.Group()
+        for label in text:
+            group.append(label)
+        display.show(group)
+        time.sleep(3)
+        group = displayio.Group()
+        display.show(group)
 
     @staticmethod
     def wakeword_callback():
@@ -131,15 +142,13 @@ class PiVoice(Thread):
             elif inference.intent == "cambiareLuminosità":      
                 self.set_led_brightness(inference.slots.get("stanza"), int(inference.slots.get("lumdec")))
                 
-            elif inference.intent == "mostraOrario":                
+            elif inference.intent == "mostraOrario":
+                self.recorder.stop()
                 now = datetime.now()
                 current_time = now.strftime("%H:%M")
-                print(current_time)
-                arguments = ["time", current_time]
-                try:
-                    self.cApp.start_c_app(arguments)
-                except subprocess.CalledProcessError as e:
-                    print(e)    
+                text_area = [label.Label(terminalio.FONT, text=current_time, scale=4, color=0xFFFFFF, x=4, y=64)]
+                self.show_text(text_area)
+                self.recorder.start()
                     
             elif inference.intent == "mostraMeteo":
                 
@@ -150,22 +159,26 @@ class PiVoice(Thread):
                     r = requests.get(url)
                     if r.status_code == 200:
                         body = r.json()['current']
-                        temp = str(body['temp_c'])
-                        print(temp)
+                        temp = str(body['temp_c']) + "C"
                         cond_code = str(body['condition']['code'] - 887)
                         path = "./pic/weather/" + cond_code + ".bmp"
-                        print(path)
-                        hum = str(body['humidity'])
-                        print(hum)
+                        hum = str(body['humidity']) + "%"
                         
-                        arguments = ["meteo", path, temp, hum]
-                        self.cApp.start_c_app(arguments)
+                        self.recorder.stop()
+                        self.show_image(path)
+                        text_area = []
+                        text_area.append(label.Label(terminalio.FONT, text="TEMP", scale=5, color=0xFFFFFF, x=4, y=32))
+                        text_area.append(label.Label(terminalio.FONT, text=temp, scale=4, color=0xFFFFFF, x=4, y=96))
+                        self.show_text(text_area)
+                        text_area = []
+                        text_area.append(label.Label(terminalio.FONT, text="HUM", scale=6, color=0xFFFFFF, x=8, y=32))
+                        text_area.append(label.Label(terminalio.FONT, text=hum, scale=6, color=0xFFFFFF, x=8, y=96))
+                        self.show_text(text_area)
+                        self.recorder.start()
                     else:
                         print(r.json())
                 except requests.ConnectionError:
-                    print("Failed to connect!")
-                except subprocess.CalledProcessError as e:
-                    print(e)                
+                    print("Failed to connect!")                
             else:
                 raise NotImplementedError()                 #Gestire
 
@@ -212,6 +225,56 @@ if __name__ == "__main__":
 
     app.run()
 
-    
+'''
+group = displayio.Group()
+display.show(group)
+color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFFFFFF  # White
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle in black
+inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0x000000  # Black
+inner_sprite = displayio.TileGrid(
+    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
+)
+splash.append(inner_sprite)
+
+# Draw some white squares
+sm_bitmap = displayio.Bitmap(8, 8, 1)
+sm_square = displayio.TileGrid(sm_bitmap, pixel_shader=color_palette, x=58, y=17)
+splash.append(sm_square)
+
+med_bitmap = displayio.Bitmap(16, 16, 1)
+med_square = displayio.TileGrid(med_bitmap, pixel_shader=color_palette, x=71, y=15)
+splash.append(med_square)
+
+lrg_bitmap = displayio.Bitmap(32, 32, 1)
+lrg_square = displayio.TileGrid(lrg_bitmap, pixel_shader=color_palette, x=91, y=28)
+splash.append(lrg_square)
+
+# Draw some label text
+text1 = "0123456789ABCDEF123456789AB"  # overly long to see where it clips
+text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=8, y=8)
+splash.append(text_area)
+text2 = "SH1107"
+text_area2 = label.Label(
+    terminalio.FONT, text=text2, scale=2, color=0xFFFFFF, x=9, y=44
+)
+splash.append(text_area2)
+bitmap = displayio.OnDiskBitmap("pic/rasplogo2.bmp")
+tile_grid = displayio.TileGrid(bitmap, pixel_shader = bitmap.pixel_shader)
+splash.append(tile_grid)
+display.show(splash)
+
+time.sleep(2)
+
+splash = displayio.Group()
+display.show(splash)
+'''
         
         
