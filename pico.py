fa7c265af1e87c9d4f30421d9aca3883f01264eb
DevozZ99 <devozzcraft@gmail.com>
2023-10-26 18:13:38 +0200

    Added AccendereSchermo and SpegnereSchermo intents

--- pico.py
+++ pico.py
@@ -5,7 +5,7 @@ import argparse
 import subprocess
 import requests
 from datetime import datetime
-from threading import Thread
+from threading import Thread, Lock
 
 from apa102 import APA102
 
@@ -44,18 +44,91 @@ ROOMS = {
 
 num_led = 3
 led_driver = APA102(num_led=num_led)
+    
+class DisplayManager:
+    def __init__(self):
+        displayio.release_displays()
+        self.i2c = board.I2C()
+        self.display_bus = displayio.I2CDisplay(self.i2c, device_address=0x3c)
+        self.width = 128
+        self.height = 128
+        self.display = adafruit_displayio_sh1107.SH1107(self.display_bus, width=self.width, height=self.height, display_offset=adafruit_displayio_sh1107.DISPLAY_OFFSET_ADAFRUIT_128x128_OLED_5297, rotation=180)
+        
+        self.show_stats = False
+        
+        self.display_lock = Lock()
+        
+    def show_image(self, path, duration=3):
+        with self.display_lock:
+            prev_show_stats = self.show_stats
+            self.show_stats = False
+            group = displayio.Group()
+            bitmap = displayio.OnDiskBitmap(path)
+            tile_grid = displayio.TileGrid(bitmap, pixel_shader = bitmap.pixel_shader)
+            group.append(tile_grid)
+            self.display.show(group)
+            if duration > 0:
+                time.sleep(duration)
+                self.reset_display()
+            self.show_stats = prev_show_stats
 
-displayio.release_displays()
-i2c = board.I2C()
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
-WIDTH = 128
-HEIGHT = 128
-display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT, display_offset=adafruit_displayio_sh1107.DISPLAY_OFFSET_ADAFRUIT_128x128_OLED_5297, rotation=180)
+    def show_text(self, text, duration=3):
+        with self.display_lock:
+            prev_show_stats = self.show_stats
+            self.show_stats = False
+            group = displayio.Group()
+            for label in text:
+                group.append(label)
+            self.display.show(group)
+            if duration > 0:
+                time.sleep(duration)
+                self.reset_display()
+            self.show_stats = prev_show_stats
+            
+    def reset_display(self):
+        group = displayio.Group()
+        self.display.show(group)
+    
+    def get_show_stats(self):
+        return self.show_stats
+    
+    def set_show_stats(self, value):
+        self.show_stats = value
 
+class StatsCollector(Thread):
+    def __init__(self, display_manager):
+        super().__init__()
+        self.display_manager = display_manager
+        
+    def run(self):
+        while True:
+            if self.display_manager.get_show_stats():
+                #CALCOLA STATS
+                cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
+                CPU = subprocess.check_output(cmd, shell = True, text=True)
+
+                cmd = "free -m | awk 'NR==2{printf \"%s/%sMB\", $3, $2}'"    
+                MemUsage = subprocess.check_output(cmd, shell = True, text=True)
+                
+                cmd = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB %s\", $3,$2,$5}'"
+                Disk = subprocess.check_output(cmd, shell = True, text=True)
+                
+                cmd = "vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1"
+                Temperature = subprocess.check_output(cmd, shell = True, text=True)
+                
+                text_area = []
+                text_area.append(label.Label(terminalio.FONT, text="CPU:"+ CPU + "%", scale=2, color=0xFFFFFF, x=4, y=25))
+                text_area.append(label.Label(terminalio.FONT, text=MemUsage, scale=2, color=0xFFFFFF, x=4, y=50))
+                text_area.append(label.Label(terminalio.FONT, text=Disk, scale=2, color=0xFFFFFF, x=4, y=75))
+                text_area.append(label.Label(terminalio.FONT, text=Temperature, scale=2, color=0xFFFFFF, x=4, y=100))
+                #CHIAMA FUNZIONE PER MOSTRARE LE STATS
+                self.display_manager.show_text(text_area, duration=0)
+            #SLEEP
+            time.sleep(1)
 
 class PiVoice(Thread):
 
-    def __init__(self, keyword_path, context_path, access_key, device_index, recorder, porcupine_sensitivity=0.75, rhino_sensitivity=0.25):
+    def __init__(self, keyword_path, context_path, access_key, device_index, recorder, display_manager, porcupine_sensitivity=0.75, rhino_sensitivity=0.25):
         super(PiVoice, self).__init__()
 
         def inference_callback(inference):
@@ -69,15 +142,11 @@ class PiVoice(Thread):
         self._default_brightness = 20
         self._device_index = device_index
         self.recorder = recorder
+        self.display_manager =  display_manager
         
         for i in range(0, num_led):
             led_driver.set_brightness(i, self._default_brightness)
-
-    @staticmethod
-    def set_color(led_index, color):
-        led_driver.set_pixel(led_index, color[0], color[1], color[2])
-        led_driver.show()
-        
+            
     def set_led_brightness(self, room, brightness):
         if room is not None:
             led_driver.set_brightness(ROOMS[room], brightness)
@@ -96,31 +165,13 @@ class PiVoice(Thread):
             for i in range(0, num_led):
                 led_driver.set_pixel(i, RGB_COLORS[new_color][0], RGB_COLORS[new_color][1], RGB_COLORS[new_color][2])
         led_driver.show()
-    
-    def show_image(self, path):
-        group = displayio.Group()
-        bitmap = displayio.OnDiskBitmap(path)
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader = bitmap.pixel_shader)
-        group.append(tile_grid)
-        display.show(group)
-        time.sleep(3)
-        group = displayio.Group()
-        display.show(group)
-        
-    def show_text(self, text):
-        group = displayio.Group()
-        for label in text:
-            group.append(label)
-        display.show(group)
-        time.sleep(3)
-        group = displayio.Group()
-        display.show(group)
 
-    @staticmethod
-    def wakeword_callback():
+    def wakeword_callback(self):
         print('[wake word]\n')
+        self.display_manager.show_image("pic/rasplogo.bmp", duration=0)
 
     def inference_callback(self, inference):
+        self.display_manager.reset_display()
         print("is_understood: " + str(inference.is_understood))
         if inference.is_understood:
             print("intent: " + str(inference.intent))
@@ -142,12 +193,23 @@ class PiVoice(Thread):
             elif inference.intent == "cambiareLuminosità":      
                 self.set_led_brightness(inference.slots.get("stanza"), int(inference.slots.get("lumdec")))
                 
+            elif inference.intent == "accendereSchermo":
+                self.recorder.stop()
+                self.display_manager.set_show_stats(True)
+                self.recorder.start()
+                
+            elif inference.intent == "spegnereSchermo":
+                self.recorder.stop()
+                self.display_manager.set_show_stats(False)
+                self.display_manager.reset_display()
+                self.recorder.start()
+                
             elif inference.intent == "mostraOrario":
                 self.recorder.stop()
                 now = datetime.now()
                 current_time = now.strftime("%H:%M")
                 text_area = [label.Label(terminalio.FONT, text=current_time, scale=4, color=0xFFFFFF, x=4, y=64)]
-                self.show_text(text_area)
+                self.display_manager.show_text(text_area)
                 self.recorder.start()
                     
             elif inference.intent == "mostraMeteo":
@@ -165,15 +227,15 @@ class PiVoice(Thread):
                         hum = str(body['humidity']) + "%"
                         
                         self.recorder.stop()
-                        self.show_image(path)
+                        self.display_manager.show_image(path)
                         text_area = []
                         text_area.append(label.Label(terminalio.FONT, text="TEMP", scale=5, color=0xFFFFFF, x=4, y=32))
                         text_area.append(label.Label(terminalio.FONT, text=temp, scale=4, color=0xFFFFFF, x=4, y=96))
-                        self.show_text(text_area)
+                        self.display_manager.show_text(text_area)
                         text_area = []
                         text_area.append(label.Label(terminalio.FONT, text="HUM", scale=6, color=0xFFFFFF, x=8, y=32))
                         text_area.append(label.Label(terminalio.FONT, text=hum, scale=6, color=0xFFFFFF, x=8, y=96))
-                        self.show_text(text_area)
+                        self.display_manager.show_text(text_area)
                         self.recorder.start()
                     else:
                         print(r.json())
@@ -214,6 +276,12 @@ if __name__ == "__main__":
                         default=-1)
 
     args = parser.parse_args()
+    
+    display_manager = DisplayManager()
+    
+    stats_thread = StatsCollector(display_manager)
+    stats_thread.daemon = True
+    stats_thread.start()
 
     recorder = None
     access_key = "gLEeR53xMGRwaC2Bd1G7xEtpA21zkSZJW2GDp1783UbsCcBJ9ceMDQ=="
@@ -221,60 +289,6 @@ if __name__ == "__main__":
                   os.path.join(os.path.dirname(__file__), 'rhino.rhn'),
                   access_key,
                   args.microphone_index,
-                  recorder)
+                  recorder, display_manager)
 
     app.run()
-
-'''
-group = displayio.Group()
-display.show(group)
-color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0xFFFFFF  # White
-
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle in black
-inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0x000000  # Black
-inner_sprite = displayio.TileGrid(
-    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
-)
-splash.append(inner_sprite)
-
-# Draw some white squares
-sm_bitmap = displayio.Bitmap(8, 8, 1)
-sm_square = displayio.TileGrid(sm_bitmap, pixel_shader=color_palette, x=58, y=17)
-splash.append(sm_square)
-
-med_bitmap = displayio.Bitmap(16, 16, 1)
-med_square = displayio.TileGrid(med_bitmap, pixel_shader=color_palette, x=71, y=15)
-splash.append(med_square)
-
-lrg_bitmap = displayio.Bitmap(32, 32, 1)
-lrg_square = displayio.TileGrid(lrg_bitmap, pixel_shader=color_palette, x=91, y=28)
-splash.append(lrg_square)
-
-# Draw some label text
-text1 = "0123456789ABCDEF123456789AB"  # overly long to see where it clips
-text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=8, y=8)
-splash.append(text_area)
-text2 = "SH1107"
-text_area2 = label.Label(
-    terminalio.FONT, text=text2, scale=2, color=0xFFFFFF, x=9, y=44
-)
-splash.append(text_area2)
-bitmap = displayio.OnDiskBitmap("pic/rasplogo2.bmp")
-tile_grid = displayio.TileGrid(bitmap, pixel_shader = bitmap.pixel_shader)
-splash.append(tile_grid)
-display.show(splash)
-
-time.sleep(2)
-
-splash = displayio.Group()
-display.show(splash)
-'''
-        
-        
